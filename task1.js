6715fd7a0fa4957613fa5dff6eb1cb6fb3c38ffd
xuananh95 <xanh.trym95@gmail.com>
2022-04-12 21:41:33 +0700

    Add comments

--- task1.js
+++ task1.js
@@ -1,12 +1,19 @@
+/**
+ * Function to return all the longest strings in an array
+ * @param {Array} inputArray - Array of input strings
+ * @returns {Array} res - Array of all the longest strings
+ */
 function allLongestStrings(inputArray){
     let n = inputArray.length;
     max = inputArray[0].length;
+    // Getting the length of the longest string in the array
     for (let i = 1; i < n; i++){
         if (max < inputArray[i].length) {
             max = inputArray[i].length;
         }
     }
     res = [];
+    // Pushing all the strings with the length equal to max into the result
     for (let i = 0; i < n; i++){
         if (inputArray[i].length == max){
             res.push(inputArray[i]);
--- task2.js
+++ task2.js
@@ -1,11 +1,19 @@
+/**
+ * Function to return the sum of odd and even position elements of an array
+ * @param {Array} a - The input numbers array
+ * @returns {Array} - An array containing the Sum of odd and even elements
+ */
 function alternatingSums(a) {
     let sum_odd = sum_even = 0;
     n = a.length;
     for (let i = 0; i < n; i++){
         if (i % 2 == 0){
+            // calculating the sum of all odd elements
+            // JS are 0-indexed, so odd elements will have their index divided by 2
             sum_odd += a[i];
         }
         else {
+            // calculating the sum of all even elements
             sum_even += a[i];
         }
     }
